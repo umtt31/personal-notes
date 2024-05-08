b069e4011bf68a0a5f7f2003e7cd973d22730692
Umutcan Adiguzel <umutcan.adiguzel@outlook.com>
2024-05-09 00:01:35 +0300

    Defining views

---
+++ resources/views/note/index.blade.php
@@ -0,0 +1,19 @@
+<x-layout>
+    <div class="note-container">
+        <a href="{{ route('note.create') }}" class="new-note-btn">New Note</a>
+        <div class="notes">
+            @foreach ($notes as $note)
+                <div class="note">
+                    <div class="note-body">
+                        {{ Str::words($note->note, 30) }}
+                    </div>
+                    <div class="note-buttons">
+                        <a href="{{ route('note.show', $note->id) }}" class="note-edit-button">View</a>
+                        <a href="{{ route('note.edit', $note->id) }}" class="note-edit-button">Edit</a>
+                        <button class="note-delete-button">Delete</button>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</x-layout>
